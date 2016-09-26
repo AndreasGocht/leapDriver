07e9bd34fd26fdeaf6cb427b4a2512fb3f995633
Andreas Gocht <andreas.gocht+git@gmail.com>
2016-09-26 08:37:53 +0200

    add license

--- src/InputInterface.cpp
+++ src/InputInterface.cpp
@@ -1,8 +1,25 @@
 /*
- * InputInterface.cpp
+ * leapDriver - a small userspace driver for the Leap Motion
  *
- *  Created on: 20.02.2016
- *      Author: andreas
+ * Copyright 2016 Andreas Gocht
+ *
+ * This file is part of leapDriver.
+ *
+ * leapDriver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * leapDriver is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with leapDriver.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ * Andreas Gocht: andreas.gocht@gmail.com
  */
 
 #include <InputInterface.h>
